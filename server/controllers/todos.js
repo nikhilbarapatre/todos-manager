78edb3c9dfb5ad476cdf6a33b7706921d164fd72
nikhilbarapatre <nbarapatre@dons.usfca.edu>
2018-09-15 19:10:26 -0700

    slack requirements done

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,8 +1,9 @@
 const Todo = require('../models').Todo;
+const TodoItem = require('../models').TodoItem;
 
 module.exports = {
 
-    //Create a todo...
+  //Create a todo...
   create(req, res) {
     return Todo
       .create({
@@ -11,4 +12,56 @@ module.exports = {
       .then(todo => res.status(201).send(todo))
       .catch(error => res.status(400).send(error));
   },
+  
+  //List a todo...
+  list(req, res) {
+    return Todo
+      .findAll({
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todos => res.status(200).send(todos))
+      .catch(error => res.status(400).send(error));
+  },
+
+  //Retrieve a single todo...
+  retrieve(req, res) {
+    return Todo
+      .findById(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return res.status(200).send(todo);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+  //Delete a todo...
+  destroy(req, res) {
+    return Todo
+      .findById(req.params.todoId)
+      .then(todo => {
+        if (!todo) {
+          return res.status(400).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .destroy()
+          .then(() => res.status(204).send({ message: 'Todo deleted successfully.' }))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
 const todosController = require('../controllers').todos;
+const todoItemsController = require('../controllers').todoItems;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
 
-  app.post('/api/todos', todosController.create);
+  app.post('/api/todos', todosController.create);//Create a todo
+  app.get('/api/todos', todosController.list);//Get the todo list
+  app.post('/api/todos/:todoId/items', todoItemsController.create);//Create a todoitem
+  app.get('/api/todos/:todoId', todosController.retrieve);//Get a todoitem
+  app.put('/api/todos/:todoId/items/:todoItemId', todoItemsController.update);//Update a todoitem
+  app.delete('/api/todos/:todoId', todosController.destroy);//Delete a todo
 };
