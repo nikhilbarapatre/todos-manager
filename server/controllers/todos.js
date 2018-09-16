64f41ce47e8601ca6a3b9fd1b1c0c93da34d7d93
nikhilbarapatre <nbarapatre@dons.usfca.edu>
2018-09-15 18:27:48 -0700

    create a todo done

---
+++ server/controllers/todos.js
@@ -0,0 +1,14 @@
+const Todo = require('../models').Todo;
+
+module.exports = {
+
+    //Create a todo...
+  create(req, res) {
+    return Todo
+      .create({
+        title: req.body.title,
+      })
+      .then(todo => res.status(201).send(todo))
+      .catch(error => res.status(400).send(error));
+  },
+};
