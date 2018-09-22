4af6063a8111f357097823e1563bd350c57479e6
nikhilbarapatre <nbarapatre@dons.usfca.edu>
2018-09-21 17:25:32 -0700

    axios test1

---
+++ todos_react/src/Components/todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Todo;
