f75858aa1e3d718cbf98bba357cc06fa656519dc
nikhilbarapatre <nbarapatre@dons.usfca.edu>
2018-09-22 17:44:29 -0700

    get request working

--- todos_react/src/Components/todo.js
+++ todos_react/src/Components/todo.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+const todosUrl = "/api/todos";
+
 class Todo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (  );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(todosUrl)
+      .then(res => this.setState({ todoList: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.todoList.map(todoList => (
+            <li key={todoList.id}>
+              {todoList.title}
+              <p />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
- 
+
 export default Todo;
