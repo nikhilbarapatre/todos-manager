30fc50cbf63ac6cc4d34a6e8202fc012a16087ba
nikhilbarapatre <nbarapatre@dons.usfca.edu>
2018-09-22 18:17:17 -0700

    creating a new todo list

--- todos_react/src/Components/todo.js
+++ todos_react/src/Components/todo.js
@@ -8,6 +8,8 @@ class Todo extends Component {
     this.state = {
       todoList: []
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -17,9 +19,32 @@ class Todo extends Component {
       .catch(err => console.log(err));
   }
 
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("todo = " + this.state.text);
+    const body = {
+      title: this.state.text
+    };
+    axios
+      .post(todosUrl, body)
+      .then(res => this.setState({ todoList: res.data.concat(body) }))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <p>Add new todo: </p>
+            <input onChange={this.handleChange} placeholder="Todo Name" />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
         <ul>
           {this.state.todoList.map(todoList => (
             <li key={todoList.id}>
